c343d46ea88f0b3bb412606992d944ed77d4fb15
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-08-13 19:21:15 +0700

    Implementasi UI dan Backend, Done

--- resources/views/Activity/activity-create.blade.php
+++ resources/views/Activity/activity-create.blade.php
@@ -1,30 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Activity Create</title>
-</head>
-<body>
+@extends('layout.dashboard', ['title' => 'Create Activity'])
+
+@section('body')
+
+<div class="p-4">
     @if ($errors->any())
-        <p style="color: red">{{ $errors->first() }}</p>
+    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-200 dark:bg-gray-800 dark:text-red-400" role="alert">
+        {{ $errors->first() }}
+    </div>
     @endif
+
     <form method="post" action="{{ route('activity.postCreate') }}" enctype="multipart/form-data">
         @csrf
-        <p>title</p>
-        <input type="text" name="title" id="" placeholder="title" value="{{ old('title') }}">
-        <p>tanggal</p>
-        <input type="date" name="tanggal" id="" placeholder="tanggal" value="{{ old('tanggal') }}">
-        <p>time start</p>
-        <input type="time" name="time_start" id="" placeholder="time_start" value="{{ old('time_start') }}">
-        <p>time finish</p>
-        <input type="time" name="time_finish" id="" placeholder="time_end" value="{{ old('time_finish') }}">
-        <p>details</p>
-        <textarea name="details" id="editor" cols="30" rows="10">{{ old('details') }}</textarea>
-        <button type="submit">Create</button>
-    </form>
-</body>
-@include('Includes.MinCKEditor')
+        <div class="mb-6">
+            <label for="title"  class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Title Activity <span class="text-red" data-tooltip-target="max-title">(Max 200)</span></label>
+            <div id="max-title" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                max word is 200
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('title') }}">
+        </div>
 
-</html>
+        <div class="mb-6">
+            <label for="tanggal" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Tanggal</label>
+            <input type="date" name="tanggal" id="tanggal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('tanggal') }}">
+        </div>
+
+        <div class="flex flex-row gap-6 mb-6">
+            <div class="w-full">
+                <label for="time_start" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Time Start</label>
+                <input type="time" name="time_start" id="time_start" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('time_start') }}">
+            </div>
+            <div class="w-full">
+                <label for="time_finish" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Time Finish</label>
+                <input type="time" name="time_finish" id="time_finish" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('time_finish') }}">
+            </div>
+        </div>
+        
+        <div class="mb-6">
+            <label for="editor" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Details Activity</label>
+            <textarea name="details" id="editor" cols="30" rows="10">{{ old('details') }}</textarea>
+        </div>
+
+        <button class="bg-blue-600 hover:bg-blue-800 text-white rounded px-2 text-md font-semibold p-1 float-right mb-6">Create Activity</button>
+    </form>
+    @include('Includes.MinCKEditor')
+</div>
+
+@endsection
--- resources/views/Activity/activity-update.blade.php
+++ resources/views/Activity/activity-update.blade.php
@@ -1,30 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Activity Create</title>
-</head>
-<body>
-    @if ($errors->any())
-        <p style="color: red">{{ $errors->first() }}</p>
-    @endif
-    <form method="post" action="{{ route('activity.postUpdate', ['id' => $activity->id]) }}" enctype="multipart/form-data">
-        @csrf
-        <p>title</p>
-        <input type="text" name="title" id="" placeholder="title" value="{{ old('title') ?? $activity->title }}">
-        <p>tanggal</p>
-        <input type="date" name="tanggal" id="" placeholder="tanggal" value="{{ old('tanggal') ?? $activity->tanggal }}">
-        <p>time start</p>
-        <input type="time" name="time_start" id="" placeholder="time_start" value="{{ old('time_start') ?? $activity->details['time-start'] }}">
-        <p>time finish</p>
-        <input type="time" name="time_finish" id="" placeholder="time_end" value="{{ old('time_finish') ?? $activity->details['time-finish']}}">
-        <p>details</p>
+@extends('layout.dashboard', ['title' => 'Edit Activity'])
+
+@section('body')
+
+
+<form method="post" action="{{ route('activity.postUpdate', ['id' => $activity->id]) }}" enctype="multipart/form-data">
+    @csrf
+    <div class="mb-6">
+        <label for="title" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Title Activity <span class="text-red">(Max 200)</span></label>
+        <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('title') ?? $activity->title }}">
+    </div>
+
+    <div class="mb-6">
+        <label for="tanggal" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Tanggal</label>
+        <input type="date" name="tanggal" id="tanggal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('tanggal') ?? $activity->tanggal }}">
+    </div>
+
+    <div class="flex flex-row gap-6 mb-6">
+        <div class="w-full">
+            <label for="time_start" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Time Start</label>
+            <input type="time" name="time_start" id="time_start" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('time_start') ?? $activity->details['time-start'] }}">
+        </div>
+        <div class="w-full">
+            <label for="time_finish" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Time Finish</label>
+            <input type="time" name="time_finish" id="time_finish" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('time_finish') ?? $activity->details['time-finish']}}">
+        </div>
+    </div>
+    
+    <div class="mb-6">
+        <label for="editor" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Details Activity</label>
         <textarea name="details" id="editor" cols="30" rows="10">{{ old('details') ?? $activity->details['details'] }}</textarea>
-        <button type="submit">Create</button>
-    </form>
-</body>
+    </div>
+
+    <button class="bg-blue-600 hover:bg-blue-800 text-white rounded px-2 text-md font-semibold p-1 float-right mb-6">Update Activity</button>
+</form>
 @include('Includes.MinCKEditor')
 
-</html>
+@endsection
