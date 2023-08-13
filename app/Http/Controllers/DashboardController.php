7884a924c5b507f48252e3115b8447d94257af02
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-08-13 19:23:18 +0700

    Display activity pada dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\ActivityService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    private ActivityService $activity;
+
     public function __construct(){
         $this->middleware(['auth', 'verified']);
+        $this->activity = new ActivityService;
     }
     public function index(){
-        return view('Dashboard/dashboard');
+        $activitys = $this->activity->activityWeek();
+        return view('Dashboard/dashboard', compact('activitys'));
     }
 
     public function profile(){
