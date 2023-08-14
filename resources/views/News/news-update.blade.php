9e1179a3310ada2938382b7274c4365d9d96cd47
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-08-14 10:53:09 +0700

    Update UI with Backend, done

--- resources/views/News/news-update.blade.php
+++ resources/views/News/news-update.blade.php
@@ -1,26 +1,62 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>News Create</title>
-</head>
-<body>
+@extends('layout.dashboard', ['title' => 'Update News'])
+
+@section('body')
+
+<div class="p-4">
     @if ($errors->any())
-        @foreach ($errors->all() as $item)
-            <p>{{ $item }}</p>
-        @endforeach
+    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-200 dark:bg-gray-800 dark:text-red-400" role="alert">
+        {{ $errors->first() }}
+    </div>
     @endif
+
     <form method="post" enctype="multipart/form-data">
         @csrf
-        <input type="text" name="title" id="" value="{{  old('title') ?? $news->title }}">
-        <input type="file" name="img-thumbnail" id="" accept="image/png, image/gif, image/jpeg">
-        <textarea name="details" id="editor" cols="30" rows="10">
-            {{ old('details') ?? $news->details }}
-        </textarea>
-        <button type="submit">Create</button>
-    </form>
-</body>
-@include('Includes.CKEditor')
+        <div class="mb-6">
+            <label for="title"  class="block mb-2 text-md font-medium text-gray-900 dark:text-white">News Title <span class="text-red" data-tooltip-target="max-title">(Max 200)</span></label>
+            <div id="max-title" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                max word is 200
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ old('title') ?? $news->title}}">
+        </div>
 
-</html>
+        <div class="mb-6">
+            <label class="block mb-2 text-md font-medium text-gray-900 dark:text-white" for="file_input">News Thumbnail</label>
+            <p>Old Thumbnail</p>
+            <img src="{{asset('image/news/thumbnail/' . $news->img)}}" alt="old thumb" class="h-48 w-96 mb-4">
+            <input class="block w-full text-sm border border-gray-300 rounded-lg cursor-pointer bg-gray-50" aria-describedby="file_input_help" name="img-thumbnail" accept="image/png, image/jpeg" id="file_input" type="file">
+            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">PNG, JPG and JPEG (MAX 2 MB).</p>
+            <div id="display-img"></div>
+        </div>
+        
+        <div class="mb-6">
+            <label for="editor" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">News Description</label>
+            <textarea name="details" id="editor" cols="30" rows="10">{{ old('details') ?? $news->details }}</textarea>
+        </div>
+
+        <button class="bg-blue-600 hover:bg-blue-800 text-white rounded px-2 text-md font-semibold p-1 float-right mb-6">Update Activity</button>
+    </form>
+    @include('Includes.CKEditor')
+</div>
+
+@endsection
+
+@section('script')
+
+<script>
+    file_input.onchange = evt => {
+        const [file] = file_input.files
+        const target = document.getElementById("display-img")
+        target.firstChild?.remove()
+
+        if (file) {
+            const img = document.createElement('img')
+            img.setAttribute("class", "h-48 w-96");
+            img.setAttribute("src", URL.createObjectURL(file))
+
+            target.append(img)
+        }
+    }
+</script>
+
+@endsection
---
+++ resources/views/News/news.blade.php
@@ -0,0 +1,101 @@
+@extends('layout.dashboard', ['title' => 'News'])
+
+@section('body')
+
+<div class="p-4">
+    <div class="flex items-center justify-between py-5">
+        <h1 class="font-bold text-blue-800 text-2xl mb-0">News List</h1>
+        <a href="{{ route('news.create') }}">
+            <button class="bg-blue-600 hover:bg-blue-800 text-white rounded px-2 text-md font-semibold p-1">Add
+                News</button>
+        </a>
+    </div>
+    <hr />
+
+    <form action="{{ route('news.searchDas') }}" method="GET">   
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" name="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+    </form>
+
+    <div class="pt-3 relative overflow-x-auto">
+        @if (session()->has('success'))
+            <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-200 dark:bg-gray-800 dark:text-green-400" role="alert">
+                {{ session()->get('success') }}
+            </div>
+        @elseif(session()->has('errors'))
+            <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-200 dark:bg-gray-800 dark:text-red-400" role="alert">
+                {{ session()->get('errors') }}
+            </div>
+        @endif
+
+        <table class="w-full text-xs md:text-sm text-left text-slate-500">
+            <thead class="text-xs text-slate-700 uppercase bg-slate-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        No
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Title
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Author
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Created At
+                    </th>
+                    <th scope="col" class="px-6 py-3 float-right">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($news as $key => $value)
+                <tr class="bg-white border-b hover:bg-gray-200">
+                    <td class="px-6 py-4">
+                        {{ ($news->currentPage() - 1) * $news->perPage() + $key + 1 }}
+                    </td>
+                    <td class="px-6 py-4 capitalize">
+                        {{ $value->title }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $value->created_by }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ date("d-m-Y, H:i:s", strtotime($value->created_at)) }}
+                    </td>
+                    <td>
+                        <div class="flex flex-row space-x-2 float-right">
+                            <a href="{{ route('news.update', ['slug' => $value->slug]) }}"
+                                class="bg-yellow-500  hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                Edit
+                            </a>
+                            <a href="{{ route('news.delete', ['slug' => $value->slug]) }}"
+                                onclick="return confirm('Apakah anda yakin menghapus data ini?')" type="button"
+                                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3.5 rounded">
+                                Delete
+                            </a>
+                        </div>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="6" class="text-center font-medium p-4">Empty Data</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+        <div class="pt-8">
+            {{ $news->links() }}
+        </div>
+    </div>
+</div>
+
+@endsection
