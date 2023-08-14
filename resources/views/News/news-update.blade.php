e8723e0839d044be1a7627efd0d11f89f67d06c0
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-08-14 19:54:42 +0700

    Penambahan input selected massage

--- resources/views/News/news-update.blade.php
+++ resources/views/News/news-update.blade.php
@@ -30,6 +30,17 @@
         </div>
         
         <div class="mb-6">
+            <label for="countries" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Commentar Massage</label>
+            <select id="countries" name="massage" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                @if ($news->id_massage)
+                    <option value="yes" selected>Yes</option>
+                @endif
+                <option value="no" selected>No</option>
+                <option value="yes">Yes</option>
+            </select>
+        </div>
+
+        <div class="mb-6">
             <label for="editor" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">News Description</label>
             <textarea name="details" id="editor" cols="30" rows="10">{{ old('details') ?? $news->details }}</textarea>
         </div>
