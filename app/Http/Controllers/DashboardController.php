1ac8e20d73246daaa9d45337ea3797a617381aff
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-08-17 11:36:52 +0700

    pemanggilan form service user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Service\ActivityService;
+use App\Service\FormService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     private ActivityService $activity;
+    private FormService $forms;
 
     public function __construct(){
         $this->middleware(['auth', 'verified']);
         $this->activity = new ActivityService;
+        $this->forms = new FormService;
     }
     public function index(){
+        $forms = $this->forms->accountFormJoined();
         $activitys = $this->activity->activityWeek();
-        return view('Dashboard/dashboard', compact('activitys'));
+        
+        return view('Dashboard/dashboard', compact('activitys', 'forms'));
     }
 
     public function profile(){
