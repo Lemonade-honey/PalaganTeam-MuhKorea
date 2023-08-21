5cd8b3fa13814d027c818b011283039e27646d35
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-08-21 12:37:46 +0700

    Penambahan validasi access

--- resources/views/Form/form.blade.php
+++ resources/views/Form/form.blade.php
@@ -89,6 +89,7 @@
                                 class="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 View
                             </a>
+                            @if ($value->created_by == Auth::user()->email || Auth::user()->role == 'admin')
                             <a href="{{ route('form.update', ['slug' => $value->slug]) }}"
                                 class="bg-yellow-500  hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
                                 Edit
@@ -98,6 +99,7 @@
                                 class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3.5 rounded">
                                 Delete
                             </a>
+                            @endif
                         </div>
                     </td>
                 </tr>
